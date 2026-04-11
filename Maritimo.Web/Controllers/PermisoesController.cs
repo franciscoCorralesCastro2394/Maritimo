286a983f4830d36538a9db1a9f9dd2c870f795a5
Francisco <chikocorrales@hotmail.com>
2026-04-10 23:43:36 -0600

    cambios en roles y permisos

--- Maritimo.Web/Controllers/PermisoesController.cs
+++ Maritimo.Web/Controllers/PermisoesController.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Maritimo.Data.Context;
+using Maritimo.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Maritimo.Models.Models;
-using Maritimo.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Maritimo.Web.Controllers
 {
-    public class RolsController : Controller
+    public class PermisoesController : Controller
     {
         private readonly MaritimoDbContext _context;
 
-        public RolsController(MaritimoDbContext context)
+        public PermisoesController(MaritimoDbContext context)
         {
             _context = context;
         }
 
-        // GET: Rols
+        // GET: Permisoes
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Roles.ToListAsync());
+            return View(await _context.Permisos.ToListAsync());
         }
 
-        // GET: Rols/Details/5
+        // GET: Permisoes/Details/5
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -33,39 +33,39 @@ namespace Maritimo.Web.Controllers
                 return NotFound();
             }
 
-            var rol = await _context.Roles
+            var permiso = await _context.Permisos
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (rol == null)
+            if (permiso == null)
             {
                 return NotFound();
             }
 
-            return View(rol);
+            return View(permiso);
         }
 
-        // GET: Rols/Create
+        // GET: Permisoes/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Rols/Create
+        // POST: Permisoes/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nombre")] Rol rol)
+        public async Task<IActionResult> Create([Bind("Id,Nombre")] Permiso permiso)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(rol);
+                _context.Add(permiso);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            return View(rol);
+            return View(permiso);
         }
 
-        // GET: Rols/Edit/5
+        // GET: Permisoes/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -73,22 +73,22 @@ namespace Maritimo.Web.Controllers
                 return NotFound();
             }
 
-            var rol = await _context.Roles.FindAsync(id);
-            if (rol == null)
+            var permiso = await _context.Permisos.FindAsync(id);
+            if (permiso == null)
             {
                 return NotFound();
             }
-            return View(rol);
+            return View(permiso);
         }
 
-        // POST: Rols/Edit/5
+        // POST: Permisoes/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Rol rol)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre")] Permiso permiso)
         {
-            if (id != rol.Id)
+            if (id != permiso.Id)
             {
                 return NotFound();
             }
@@ -97,12 +97,12 @@ namespace Maritimo.Web.Controllers
             {
                 try
                 {
-                    _context.Update(rol);
+                    _context.Update(permiso);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!RolExists(rol.Id))
+                    if (!PermisoExists(permiso.Id))
                     {
                         return NotFound();
                     }
@@ -113,10 +113,10 @@ namespace Maritimo.Web.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            return View(rol);
+            return View(permiso);
         }
 
-        // GET: Rols/Delete/5
+        // GET: Permisoes/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -124,34 +124,34 @@ namespace Maritimo.Web.Controllers
                 return NotFound();
             }
 
-            var rol = await _context.Roles
+            var permiso = await _context.Permisos
                 .FirstOrDefaultAsync(m => m.Id == id);
-            if (rol == null)
+            if (permiso == null)
             {
                 return NotFound();
             }
 
-            return View(rol);
+            return View(permiso);
         }
 
-        // POST: Rols/Delete/5
+        // POST: Permisoes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var rol = await _context.Roles.FindAsync(id);
-            if (rol != null)
+            var permiso = await _context.Permisos.FindAsync(id);
+            if (permiso != null)
             {
-                _context.Roles.Remove(rol);
+                _context.Permisos.Remove(permiso);
             }
 
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool RolExists(int id)
+        private bool PermisoExists(int id)
         {
-            return _context.Roles.Any(e => e.Id == id);
+            return _context.Permisos.Any(e => e.Id == id);
         }
     }
 }
--- Maritimo.Web/Program.cs
+++ Maritimo.Web/Program.cs
@@ -1,9 +1,11 @@
+using Maritimo.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Maritimo.Web.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<MaritimoWebContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("MaritimoWebContext") ?? throw new InvalidOperationException("Connection string 'MaritimoWebContext' not found.")));
+builder.Services.AddDbContext<MaritimoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MaritimoDbContext") ?? throw new InvalidOperationException("Connection string 'MaritimoDbContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
